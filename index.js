cf99a10159be17a7219867556b061773d99aabb2
kaushani <kaushanichowdhury52@gmail.com>
2023-05-18 07:15:11 +0530

    add

--- index.js
+++ index.js
@@ -23,6 +23,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("toymarketDB");
+    const dollsCollection = database.collection("dolls");
+
+    app.post("/dolls", async (req, res) => {
+      const tollsData = req.body;
+      const result = await dollsCollection.insertOne(tollsData);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
